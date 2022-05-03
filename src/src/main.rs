f9c03d0bbf29cb2a683cf760165a1645f82b1498
Philipp Groß <philipp.gross92@outlook.com>
2022-05-03 21:31:04 +0200

    multiple dice + sum

--- src/src/main.rs
+++ src/src/main.rs
@@ -9,16 +9,38 @@ fn main() {
     print!("Seitenzahl eingeben: ");
     io::stdout().flush().unwrap();
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Keine Eingabe");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Keine Eingabe der Würfelart");
 
     //Eingabebearbeitung
-    let parse_result: Result<u32, ParseIntError> = input.trim().parse::<u32>();
+    let mut parse_result: Result<u32, ParseIntError> = input.trim().parse::<u32>();
     match parse_result {
         Ok(number) => {
             let mut roller: Roller = Roller::new(Box::new(Hc128RngEngine::new()));
-            let dice: Dice = roller.roll_dice(number);
-            println!("Du würfelst einen {} seitigen Würfel", number);
-            println!("Das Würfelergebnis ist: {} von {}", dice.value, dice.sides);
+            print!("Würfelanzahl eingeben: ");
+            io::stdout().flush().unwrap();
+            input.clear();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Keine Eingabe der Anzahl");
+            parse_result = input.trim().parse::<u32>();
+
+            match parse_result {
+                Ok(count) => {
+                    println!("Du würfelst {} {} seitige Würfel", count, number);
+                    let mut x = 0;
+                    let mut sum = 0;
+                    while x < count {
+                        let dice: Dice = roller.roll_dice(number);
+                        println!("Das Würfelergebnis ist: {} von {}", dice.value, dice.sides);
+                        sum = sum + dice.value;
+                        x = x + 1;
+                    }
+                    println!("Die Augensumme beträgt {}", sum);
+                }
+                Err(_countfail) => println!("Würfelanzahl falsch"),
+            }
         }
         Err(_fail) => println!("Wieviele numerische Seiten hat der Würfel, du Pfogel?"),
     }
